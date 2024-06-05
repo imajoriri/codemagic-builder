146d73bc79d6f2125771ee0e1b5fc01bd423bb9c
imajoriri <imajo.srroni@gmail.com>
2024-06-05 09:46:56 +0900

    select oneの改善

--- lib/controller/select_one/select_one.dart
+++ lib/controller/select_one/select_one.dart
@@ -1,4 +1,4 @@
-import 'package:dart_console/dart_console.dart';
+import 'package:interact/interact.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'select_one.g.dart';
@@ -12,38 +12,9 @@ String selectOne(
   required String title,
   required List<String> options,
 }) {
-  final console = Console();
-  var selectedIndex = 0;
-
-  void renderMenu() {
-    console.clearScreen();
-    console.writeLine(title);
-    for (var i = 0; i < options.length; i++) {
-      if (i == selectedIndex) {
-        console.setForegroundColor(ConsoleColor.white);
-        console.setBackgroundColor(ConsoleColor.blue);
-        console.writeLine(options[i]);
-        console.resetColorAttributes();
-      } else {
-        console.writeLine(options[i]);
-      }
-    }
-  }
-
-  renderMenu();
-
-  while (true) {
-    final key = console.readKey();
-
-    if (key.controlChar == ControlCharacter.arrowDown) {
-      selectedIndex = (selectedIndex + 1) % options.length;
-    } else if (key.controlChar == ControlCharacter.arrowUp) {
-      selectedIndex = (selectedIndex - 1 + options.length) % options.length;
-    } else if (key.controlChar == ControlCharacter.enter) {
-      console.clearScreen();
-      return options[selectedIndex];
-    }
-
-    renderMenu();
-  }
+  final selection = Select(
+    prompt: title,
+    options: options,
+  ).interact();
+  return options[selection];
 }
